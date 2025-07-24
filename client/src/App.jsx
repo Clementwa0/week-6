86649c6508705325e72764fe42836c3d24a28a2a
clementwa0 <clementwa01@gmail.com>
2025-07-24 20:43:41 +0300

    implement Sum test functionality

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Button} from "@/components/ui/button"
+function sum(a, b) {
+  return a + b;
+}
+module.exports = sum;
+
+const App = () => {
+  return (
+    <div>
+
+      <Button>
+        link
+      </Button>
+    </div>
+  )
+}
+
+export default App
--- client/src/tests/unit/Button.test.jsx
+++ client/src/tests/unit/Button.test.jsx
@@ -1,9 +1,7 @@
-// Button.test.jsx - Unit test for Button component
-
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Button from '../../components/Button';
+import Button from '../../components/ui/button';
 
 describe('Button Component', () => {
   // Test rendering
